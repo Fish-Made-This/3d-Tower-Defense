7083b558fb3c6d81d109cf129520cb3742090f58
FishSt1ck <66172388+FishSt1ck-git@users.noreply.github.com>
2020-08-08 21:23:38 +0200

    Added enemySpawner
    - Added waypoints
    - Added waveSpawner
    - Added enemy movement
    - Added waveTimer
    - Added fonts

---
+++ 3d Tower Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 15f;
+
+    private Transform _target;
+    private int _wayPointIndex;
+
+    void Start()
+    {
+        _target = Waypoints.waypoints[_wayPointIndex];
+    }
+
+    void Update()
+    {
+        var direction = _target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(_target.position, transform.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (_wayPointIndex == Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _wayPointIndex++;
+            _target = Waypoints.waypoints[_wayPointIndex];
+        }        
+    }
+}
---
+++ 3d Tower Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public Text waveTimer;
+    public float timeBetweenWaves = 5.5f;
+
+    private float countDown = 2f;
+    private int waveIndex = 0;
+    
+    void Update()
+    {
+        waveTimer.text = $"Next wave: {Math.Round(countDown)} seconds";
+        if (countDown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+        }        
+        countDown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i <= waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.2f);
+        }
+        waveIndex++;
+    }
+
+    void SpawnEnemy()
+    {
+        Debug.Log("Spawning Enemy!");
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
