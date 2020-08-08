9a1e0d4931fbf2e2fc1b9c71b3d59774b91294ec
FishSt1ck <66172388+FishSt1ck-git@users.noreply.github.com>
2020-08-08 22:34:59 +0200

    Added turret
    - Added turret model
    - Added turret targeting/rotation

---
+++ 3d Tower Defense/Assets/Scripts/Turret.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public float range = 15f;
+    public float turnSpeed = 10f;
+    public string enemyTag = "Enemy";
+    public Transform partToRotate;
+
+    private Transform target;
+
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (var enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (target == null) { return; }
+
+        var dir = target.position - transform.position;
+        var lookRotation = Quaternion.LookRotation(dir);
+        var rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+}
